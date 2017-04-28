b1a0ebcce7f1edf0b9751a99a26afba969a11071
manhbv <dunglt@bip.com.vn>
2017-04-28 08:44:13 +0700

    ManhBV: OverTimeService.groovy

--- grails-app/services/book/OverTimeService.groovy
+++ grails-app/services/book/OverTimeService.groovy
@@ -17,9 +17,30 @@ class OverTimeService {
         return roundedNumber
     }
 
+    float getOverTimeUsingCalendar(String year, String month, String day, String comeTime, String leaveTime) throws Exception {
+        StringBuilder comeDate = new StringBuilder()
+        StringBuilder leaveDate = new StringBuilder()
+
+        comeDate.append(year)
+        leaveDate.append(year)
+
+        if (month.length() == 1) month = String.format("0%s", month)
+        comeDate.append("-").append(month)
+        leaveDate.append("-").append(month)
+
+        if (day.length() == 1) day = String.format("0%s", day)
+        comeDate.append("-").append(day)
+        leaveDate.append("-").append(day)
+
+        comeDate.append(" ").append(comeTime)
+        leaveDate.append(" ").append(leaveTime)
+
+        return getOverTimeUsingCalendar(comeDate.toString(), leaveDate.toString())
+    }
+
     float getOverTimeUsingCalendar(String comeDate, String leaveDate) {
-//        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm")
-        DateFormat df = new SimpleDateFormat("HH:mm")
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm")
+//        DateFormat df = new SimpleDateFormat("HH:mm")
         Calendar calComeDate = Calendar.getInstance()
         Calendar calLeaveDate = Calendar.getInstance()
         final int NINE_HOURS = 9
@@ -38,7 +59,12 @@ class OverTimeService {
             float leavedateHours = leaveDarteInMiliseconds / (60 * 1000 * 60) % 60
             float diff = leavedateHours - comdateHours
 
-            overTimeInHours = diff - NINE_HOURS
+            // sunday case
+            if (calLeaveDate.get(Calendar.DAY_OF_WEEK) != 1) {
+                overTimeInHours = diff - NINE_HOURS
+            } else if (calLeaveDate.get(Calendar.HOUR_OF_DAY) > 13) {
+                overTimeInHours = diff - 1
+            }
 
             if (overTimeInHours < 0) return 0
 
@@ -80,11 +106,15 @@ class OverTimeService {
         // if HourOfLeaveDate > 19:15 aproate round
         roundHourOfLeaveDate(calendar)
 
+        // if DAY_OF_WEEK == 1(SUNDAY) and HOUR_OF_DAY = 12 then set minute = 0
+        if ((calendar.get(Calendar.HOUR_OF_DAY) == 12) && (calendar.get(Calendar.DAY_OF_WEEK) == 1)) {
+            calendar.set(calendar.get(Calendar.MINUTE), 0)
+            return
+        }
+
         // if      minute leave < 15 then round to 30 and hour -1
         // else if minute leave < 45 then round to 0
         // else    round to 30
-        int hour = calendar.get(Calendar.HOUR_OF_DAY)
-        int minute = calendar.get(Calendar.MINUTE)
         if (calendar.get(Calendar.MINUTE) < 15) {
             calendar.set(Calendar.MINUTE, 30)
             calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) - 1)
@@ -100,7 +130,7 @@ class OverTimeService {
         int hour = calendar.get(Calendar.HOUR_OF_DAY)
         int minute = calendar.get(Calendar.MINUTE)
         if (calendar.get(Calendar.HOUR_OF_DAY) >= 20) {
-            if (calendar.get(Calendar.MINUTE) < 15){
+            if (calendar.get(Calendar.MINUTE) < 15) {
                 calendar.set(Calendar.MINUTE, 15)
             }
             calendar.set(Calendar.HOUR_OF_DAY, calendar.get(Calendar.HOUR_OF_DAY) - 1)
